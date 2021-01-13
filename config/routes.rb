38fd27e77bd337be0b1087683dbdf6bc85e76929
kuritaseiya <newseiya0222@icloud.com>
2021-01-14 03:23:14 +0900

    実装

--- config/routes.rb
+++ config/routes.rb
@@ -1,5 +1,5 @@
 Rails.application.routes.draw do
   devise_for :users
-  resources :plan, only: [:index,:new,:create]
   root to: "entrances#index"
+  resources :plan, only: [:index,:new,:create]
 end
