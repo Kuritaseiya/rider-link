7ea896eefa4b4fa3205841a860c27f5d74bbbf5d
kuritaseiya <newseiya0222@icloud.com>
2021-01-20 21:25:39 +0900

    データベース実装

--- db/schema.rb
+++ db/schema.rb
@@ -10,7 +10,17 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 2021_01_10_100827) do
+ActiveRecord::Schema.define(version: 2021_01_16_184134) do
+
+  create_table "messages", options: "ENGINE=InnoDB DEFAULT CHARSET=utf8", force: :cascade do |t|
+    t.text "text"
+    t.bigint "user_id"
+    t.bigint "plan_id"
+    t.datetime "created_at", precision: 6, null: false
+    t.datetime "updated_at", precision: 6, null: false
+    t.index ["plan_id"], name: "index_messages_on_plan_id"
+    t.index ["user_id"], name: "index_messages_on_user_id"
+  end
 
   create_table "plans", options: "ENGINE=InnoDB DEFAULT CHARSET=utf8", force: :cascade do |t|
     t.string "when"
@@ -39,5 +49,7 @@ ActiveRecord::Schema.define(version: 2021_01_10_100827) do
     t.index ["reset_password_token"], name: "index_users_on_reset_password_token", unique: true
   end
 
+  add_foreign_key "messages", "plans"
+  add_foreign_key "messages", "users"
   add_foreign_key "plans", "users"
 end
