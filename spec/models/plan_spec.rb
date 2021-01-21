afd90b73c6d556c2f9dc7a5d34ee1dfa212934b1
kuritaseiya <newseiya0222@icloud.com>
2021-01-21 11:07:21 +0900

    planテストコード実装

--- spec/models/plan_spec.rb
+++ spec/models/plan_spec.rb
@@ -1,5 +1,45 @@
 require 'rails_helper'
 
 RSpec.describe Plan, type: :model do
-  pending "add some examples to (or delete) #{__FILE__}"
+  before do
+    @plan = FactoryBot.build(:plan)
+  end
+
+  describe 'ツーリングプラン企画機能：正常系' do
+    it "全ての欄を埋めて指定された条件を満たしている時" do
+      expect(@plan).to be_valid
+    end
+  end
+
+  describe 'ツーリングプラン企画機能：異常系' do
+    it "year_idが1の場合では登録できない" do
+      @plan.year_id = 1
+      @plan.valid?
+      expect(@plan.errors.full_messages).to include("Year must be other than 1")
+    end
+
+    it "moon_idが1の場合では登録できない" do
+      @plan.moon_id = 1
+      @plan.valid?
+      expect(@plan.errors.full_messages).to include("Moon must be other than 1")
+    end
+
+    it "moon_idが1の場合では登録できない" do
+      @plan.day_id = 1
+      @plan.valid?
+      expect(@plan.errors.full_messages).to include("Day must be other than 1")
+    end
+
+    it "prefecture_idが1の場合では登録できない" do
+      @plan.prefecture_id = 1
+      @plan.valid?
+      expect(@plan.errors.full_messages).to include("Prefecture must be other than 1")
+    end
+
+    it "conditionsが空の場合では登録できない" do
+      @plan.conditions = ""
+      @plan.valid?
+      expect(@user.errors.full_messages).to include("Conditions can't be blank")
+    end
+  end
 end
